cad730f58f2a7add1d3b80d7f75baeed74f67505
sagar Bhalekar <sagar.bhalekar@ebixcash.com>
2021-07-16 21:05:16 +0530

    update

--- app/src/main/java/com/test/allkotlin/MainActivity.kt
+++ app/src/main/java/com/test/allkotlin/MainActivity.kt
@@ -25,7 +25,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-
+//Updated on 16072021
         val dao = SubscriberDatabase.getInstance(application).subscriberDao
         val repository = SubscriberRepository(dao)
         val factory = SubscriberViewModelFactory(repository)
